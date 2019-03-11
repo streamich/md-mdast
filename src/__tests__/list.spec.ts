0c85c89e261ef72431df3aa9e0a608cd4341f6b7
streamich <streamich@gmail.com>
2019-03-11 20:55:26 +0100

    test: 💍 add tests for list

--- src/__tests__/list.spec.ts
+++ src/__tests__/list.spec.ts
@@ -1,5 +1,502 @@
 import {create} from '../index';
 
+const tests = [
+    {
+        name: 'single item',
+        md: `- foo`,
+        ast: {
+            type: 'root',
+            children: [
+                {
+                    type: 'list',
+                    ordered: false,
+                    children: [
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'foo',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        name: 'two items',
+        md: `- foo
+- bar`,
+        ast: {
+            type: 'root',
+            children: [
+                {
+                    type: 'list',
+                    children: [
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'foo',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'bar',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        name: 'three items',
+        md: `- foo
+- bar
+- baz`,
+        ast: {
+            type: 'root',
+            children: [
+                {
+                    type: 'list',
+                    children: [
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'foo',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'bar',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'baz',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        name: 'three items - allows two leading spaces',
+        md: `  - foo
+  - bar
+  - baz`,
+        ast: {
+            type: 'root',
+            children: [
+                {
+                    type: 'list',
+                    children: [
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'foo',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'bar',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'baz',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        name: 'supports nesting',
+        md: `- Item 1
+- Item 2
+  - Item 2.1
+  - Item 2.2
+     - Item 2.2.1
+- Item 3`,
+        ast: {
+            type: 'root',
+            children: [
+                {
+                    type: 'list',
+                    children: [
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'Item 1',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'Item 2',
+                                        },
+                                    ],
+                                },
+                                {
+                                    type: 'list',
+                                    children: [
+                                        {
+                                            type: 'listItem',
+                                            children: [
+                                                {
+                                                    type: 'paragraph',
+                                                    children: [
+                                                        {
+                                                            type: 'text',
+                                                            value: 'Item 2.1',
+                                                        },
+                                                    ],
+                                                },
+                                            ],
+                                        },
+                                        {
+                                            type: 'listItem',
+                                            children: [
+                                                {
+                                                    type: 'paragraph',
+                                                },
+                                                {
+                                                    type: 'list',
+                                                    children: [
+                                                        {
+                                                            type: 'listItem',
+                                                            children: [
+                                                                {
+                                                                    type: 'paragraph',
+                                                                    children: [
+                                                                        {
+                                                                            type: 'text',
+                                                                            value: 'Item 2.2.1',
+                                                                        },
+                                                                    ],
+                                                                },
+                                                            ],
+                                                        },
+                                                    ],
+                                                },
+                                            ],
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'Item 3',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        name: 'single ordered list item',
+        md: `1. foo`,
+        ast: {
+            type: 'root',
+            children: [
+                {
+                    type: 'list',
+                    ordered: true,
+                    children: [
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'foo',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        name: 'two ordered list items',
+        md: `1. foo
+2. bar`,
+        ast: {
+            type: 'root',
+            children: [
+                {
+                    type: 'list',
+                    ordered: true,
+                    children: [
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'foo',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'bar',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        name: 'two ordered list items - 1. as bullet',
+        md: `1. foo
+1. bar`,
+        ast: {
+            type: 'root',
+            children: [
+                {
+                    type: 'list',
+                    ordered: true,
+                    children: [
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'foo',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'bar',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        name: 'three ordered list items',
+        md: `1. foo
+2. bar
+3. baz`,
+        ast: {
+            type: 'root',
+            children: [
+                {
+                    type: 'list',
+                    ordered: true,
+                    children: [
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'foo',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'bar',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            type: 'listItem',
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'text',
+                                            value: 'baz',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        name: 'can have paragraphs in list',
+        md: `- paragraph 1
+
+  paragraph 2
+
+  paragraph 3
+        `,
+        ast: {
+            type: 'root',
+            children: [
+                {
+                    type: 'list',
+                    children: [
+                        {
+                            type: 'listItem',
+                            children: [{type: 'paragraph'}, {type: 'paragraph'}, {type: 'paragraph'}],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+];
+
 describe('list', () => {
     test('works for a single unordered list item', () => {
         const parser = create();
@@ -331,5 +828,16 @@ describe('list', () => {
                 },
             ],
         });
+    });
+
+    describe('automated', () => {
+        for (const {name, md, ast} of tests) {
+            test(name, () => {
+                const parser = create();
+                const result = parser.tokenizeBlock(md);
+
+                expect(result).toMatchObject(ast);
+            });
+        }
     });
 });
